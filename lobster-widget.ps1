a13ed27b3fbbed9694b91c3dc3e6c8baf2642df8
donghaozhang <zdhpeter@gmail.com>
2026-02-28 03:10:12 +1100

    feat: add desktop widget launcher + responsive layout

---
+++ lobster-widget.ps1
@@ -0,0 +1,7 @@
+﻿# Lobster Office - Desktop Widget
+# Opens as a small always-on-top-like Chrome window
+Start-Process "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList @(
+    "--app=http://127.0.0.1:18891",
+    "--window-size=360,300",
+    "--window-position=1520,720"
+)
